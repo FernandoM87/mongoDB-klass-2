4e464e4fb1e069a5d598a091c9a8ce28e114f105
FernandoM87 <marroquinf656@live.com>
2023-04-19 09:29:55 +0200

    second commit

--- test1.js
+++ test1.js
@@ -1,4 +1,4 @@
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 
 const connectionUrl = "mongodb://127.0.0.1";
 const client = new MongoClient(connectionUrl);
@@ -11,15 +11,35 @@ async function main(){
     const db = client.db(dbName);
     const collection = db.collection("cars");
 
-    const findResult = await collection.find({}).toArray();
+    /* await addNewCar(collection) */
+    await updateCar (collection);
+    await findAllCars(collection);
 
-    console.log({result});
 
     return "done!";
 }
 
-async function addNewCar(){
-    
+async function addNewCar(collection){
+    const newCar = {
+        make: "Saab",
+        model: "9000",
+        year: 1999
+    };
+
+    const result = await collection.insertOne(newCar);
+
+    console.log({result});
+}
+
+async function findAllCars(collection){
+    const findResult = await collection.find({}).toArray();
+    console.log({findResult});
+}
+
+async function updateCar (collection) {
+    const objectId = new ObjectId("6439413c8286730e85fce367")
+    const updateResult = await collection.updateOne({ _id: objectId }, {$set: { distance: 10000 }});
+    console.log({updateResult});
 }
 
 main ()
